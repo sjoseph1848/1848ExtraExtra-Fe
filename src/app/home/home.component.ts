f748e2982a4329e41a505d02320ddc1c041009a8
sjoseph1848 <sjoseph1848@gmail.com>
2020-05-25 14:43:21 -0500

    added a placeholder image for showcase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,6 @@ export class HomeComponent implements OnInit {
   // tslint:disable-next-line: max-line-length
   mainStorySubTitle = 'If you\'re still looking for your stimulus payment, we\'ll explain how to use the IRS tracking app to check its schedule status.';
   // tslint:disable-next-line: max-line-length
-  mainStoryImg = 'https://cnet3.cbsistatic.com/img/jAtXFV989_BT20FvXJ697AcGA00=/756x567/2020/04/15/5824396a-afee-430a-8250-3807ec44eab0/money-bills-wallet-coins-dollars-2.jpg';
   mainStoryLink = 'https://www.cnet.com/personal-finance/how-to-track-your-stimulus-check-now-with-the-irs-get-my-payment-app/';
 
   constructor() { }
