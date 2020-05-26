443eafb09950f1570b3a84bed6b46f47a78a49b3
sjoseph1848 <sjoseph1848@gmail.com>
2020-05-26 08:29:35 -0500

    added article section

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShowcaseComponent } from './showcase/showcase.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 
 
 @NgModule({
-  declarations: [ShowcaseComponent],
+  declarations: [ShowcaseComponent, ArticlesComponent],
   imports: [
     CommonModule
   ],
-  exports: [ShowcaseComponent]
+  exports: [ShowcaseComponent,ArticlesComponent]
 })
 export class SharedModule { }
