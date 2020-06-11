5462fee39c8552dc048e4742fc5ee870a3173f62
sjoseph1848 <sjoseph1848@gmail.com>
2020-06-10 20:18:52 -0500

    sports page is done

--- src/app/sports/sports-home/sports-home.component.ts
+++ src/app/sports/sports-home/sports-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsDataService } from '../../data/news-data.service';
 
 @Component({
   selector: 'app-sports-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sports-home.component.scss']
 })
 export class SportsHomeComponent implements OnInit {
+mainStoryTitle = 'How to track your stimulus check now with the IRS Get My Payment app';
+// tslint:disable-next-line: max-line-length
+mainStorySubTitle = 'If you\'re still looking for your stimulus payment, we\'ll explain how to use the IRS tracking app to check its schedule status.';
+// tslint:disable-next-line: max-line-length
+mainStoryLink = 'https://www.cnet.com/personal-finance/how-to-track-your-stimulus-check-now-with-the-irs-get-my-payment-app/';
 
-  constructor() { }
+otherNews = [];
+constructor(private newsDataService: NewsDataService) { }
 
-  ngOnInit() {
+ngOnInit() {
+  const topStory = this.newsDataService.getNewsByCategory('sports');
+  topStory.subscribe((data: any) => {
+    this.mainStoryTitle = data.articles[1].title;
+    this.mainStorySubTitle = data.articles[1].description;
+    this.mainStoryLink = data.articles[1].url;
+    this.otherNews = data.articles;
+    });
   }
-
 }
--- src/app/sports/sports.module.ts
+++ src/app/sports/sports.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { SportsRoutingModule } from './sports-routing.module';
 import { SportsHomeComponent } from './sports-home/sports-home.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SportsHomeComponent],
   imports: [
     CommonModule,
-    SportsRoutingModule
+    SportsRoutingModule,
+    SharedModule,
   ]
 })
 export class SportsModule { }
