d5716e52fbca9a40c0125531e4c9cf0356bfa087
sjoseph1848 <sjoseph1848@gmail.com>
2020-06-10 19:55:27 -0500

    economics page complete for now

--- src/app/economics/economics.module.ts
+++ src/app/economics/economics.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { EconomicsRoutingModule } from './economics-routing.module';
 import { EconomicsHomeComponent } from './economics-home/economics-home.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [EconomicsHomeComponent],
   imports: [
     CommonModule,
-    EconomicsRoutingModule
+    EconomicsRoutingModule,
+    SharedModule,
   ]
 })
 export class EconomicsModule { }
