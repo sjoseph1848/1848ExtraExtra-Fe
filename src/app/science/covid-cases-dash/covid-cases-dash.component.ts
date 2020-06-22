63f9097458bdc9daf5b7ebd811eda93cb2dcf79c
sjoseph1848 <sjoseph1848@gmail.com>
2020-06-21 22:31:21 -0500

    added the charts for cases

--- src/app/science/covid-cases-dash/covid-cases-dash.component.ts
+++ src/app/science/covid-cases-dash/covid-cases-dash.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CovidDataService } from '../../data/covid-data.service';
+import * as Highcharts from 'highcharts';
+
+declare var require: any;
+let Boost = require('highcharts/modules/boost');
+let noData = require('highcharts/modules/no-data-to-display');
+let More = require('highcharts/highcharts-more');
+
+Boost(Highcharts);
+noData(Highcharts);
+More(Highcharts);
+noData(Highcharts);
 
 @Component({
   selector: 'app-covid-cases-dash',
@@ -6,10 +18,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./covid-cases-dash.component.scss']
 })
 export class CovidCasesDashComponent implements OnInit {
+  county = 'Dallas';
+  public options: any = {
+    chart: {
+      type: 'spline'
+    },
+    title: {
+      text: `${this.county} Covid-19 Cases Over Time`
+    },
+    xAxis: {
+      categories:[]
+      },
+    yAxis: {
+      title: {
+        text: "Cases"
+      }
+    },
+    series: [
+      {
+        name: this.county,
+        turboThreshold: 500000,
+        data: []
+      }
+    ]
+  }
 
-  constructor() { }
+  constructor(private covid: CovidDataService) { }
 
   ngOnInit() {
+    const county = this.covid.getCovidCases(this.county);
+    county.subscribe((data: any) => {
+      this.options.xAxis.categories = data.map(dat => dat.date);
+      this.options.series[0]['data'] = data.map(dat => dat.cases);
+      Highcharts.chart('cases', this.options);
+    });
   }
 
 }
